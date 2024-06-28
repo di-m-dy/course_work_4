0e0b3026e22e2ea4fa55fadd7067bea3d612850b
di-m-dy <dimdy11.11@gmail.com>
2024-06-28 12:36:27 +0200

    add Find Classes

--- main.py
+++ main.py
@@ -3,15 +3,9 @@ en: Main file of the project for running the application.
 ru: Главный файл проекта для запуска приложения.
 """
 
-from src.api_parser import VacancyApiHH
-
 
 def run():
-    vacancies = VacancyApiHH()
-    vacancies.set_parameters(period=2, text="Python Junior", search_field='name')
-    data = vacancies.get_data()
-    for i in data['items']:
-        print(i['name'])
+    pass
 
 
 if __name__ == "__main__":
--- src/api_parser.py
+++ src/api_parser.py
@@ -12,59 +12,73 @@ from abc import ABC, abstractmethod
 import requests
 import json
 
+from api_schemes import FindVacancySchema
 
-class VacancyApiHH:
-    """
-    Description
-    """
-    def __init__(self):
-        self.url = "https://api.hh.ru/vacancies/"
-        self.parameters = {
-            "per_page": None,
-            "page": None,
-            "text": None,
-            "search_field": None,
-            "experience": None,
-            "employment": None,
-            "schedule": None,
-            "area": None,
-            "industry": None,
-            "employer_id": None,
-            "currency": None,
-            "salary": None,
-            "label": None,
-            "only_with_salary": None,
-            "period": None,
-            "date_from": None,
-            "date_to": None,
-        }
+
+class Api(ABC):
+    @abstractmethod
+    def _query(self):
+        pass
+
+
+class ApiBase(Api):
+    def __init__(self, scope):
+        self.scope = scope
+
+    def _query(self):
+        api_response = requests.get(self.scope)
+        if self.check_response(api_response):
+            return api_response.json()
+        return
+
+    @staticmethod
+    def check_response(response):
+        status = response.status_code
+        if status == 200:
+            return True
+        elif status == 402:
+            raise ValueError("Invalid response 402")
+        elif status == 404:
+            raise ValueError("Invalid response 404")
+        elif status == 403:
+            raise ValueError("Invalid response 403")
+        else:
+            raise ValueError("Invalid response")
+
+
+class ApiFind(ApiBase):
+    def __init__(self, scope):
+        super().__init__(scope)
+        self._parameters = {}
+
+    @property
+    def parameters(self):
+        if not self._parameters:
+            return
+        return {key: value for key, value in self._parameters.items() if value}
 
     def set_parameters(self, **params):
-        for param in params:
-            if param not in self.parameters:
-                raise AttributeError(
-                    f"Invalid Attribute: {param}\n"
-                    f"It must be: {', '.join(self.parameters)}"
-                )
-            else:
-                self.parameters[param] = params[param]
+        self._parameters |= params
 
-    def get_parameters(self):
-        return {key: value for key, value in self.parameters.items() if value}
-
-    def get_data(self):
-        get_requests = requests.get(self.url, params=self.get_parameters())
-        if get_requests.status_code == 200:
+    def _query(self):
+        get_requests = requests.get(self.scope, params=self.parameters)
+        if self.check_response(get_requests):
             return get_requests.json()
-        else:
-            error = get_requests.json()
-            raise ValueError(f"{error['error']['type']}")
+        return
+
+    def find(self):
+        return self._query()
 
 
-class EmployerApiHH:
+class ApiFindVacancy(ApiFind, FindVacancySchema):
+    def __init__(self):
+        super().__init__("https://api.hh.ru/vacancies")
+
+
+class ApiFindEmployer(ApiFind):
+    def __init__(self):
+        super().__init__("https://api.hh.ru/employers")
+
+
+class ApiInfoBase(ApiBase):
     pass
-
-
-
-
-
